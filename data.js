e0e0f4ed8ed33bfca3262e69d6a7b8f329767fff
StephanPagel <stephan.pagel84@gmail.com>
2022-07-06 13:05:15 +0200

    start

---
+++ data.js
@@ -0,0 +1,22 @@
+const navArray = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Team",
+    url: "/team",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Contact",
+    url: "/contact",
+  },
+];
+
+module.exports = {
+  navArray,
+};
